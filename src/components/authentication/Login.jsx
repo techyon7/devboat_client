a8d50366374101e785f8a27d2641bbc82e2ab162
mkd63 <42798903+mkd63@users.noreply.github.com>
2020-04-29 14:53:57 +0530

    login design improved

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -23,8 +23,11 @@ export default function Login() {
   const [state, setState] = React.useState("");
   // React States
   const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-
+  //const [password, setPassword] = React.useState("");
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false
+  });
   // Event Handlers
 
   //
@@ -41,7 +44,7 @@ export default function Login() {
   // const handleMouseDownPassword = event => {
   //   event.preventDefault();
   // };
-  const handleLogin = () => {
+  const handleLogin = password => {
     console.log("run");
     let body = JSON.stringify({
       email: email,
@@ -62,28 +65,120 @@ export default function Login() {
         console.log(user.name);
       });
   };
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  //
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  //
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
   // JSX Markup
   return (
     <div className={classes.root}>
       {/* Email Input */}
+      <TextField
+        fullWidth
+        label="Email"
+        className={clsx(classes.margin, classes.textField)}
+        type="email"
+        autoComplete="current-email"
+        margin="normal"
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      {/* Password Input Container */}
+      <FormControl
+        fullWidth
+        className={clsx(classes.margin, classes.textField)}
+      >
+        {/* Password Input Label */}
+        <InputLabel htmlFor="login-password">Password</InputLabel>
+
+        {/* Password Input Field */}
+        <Input
+          id="login-password"
+          type={values.showPassword ? "text" : "password"}
+          value={values.password}
+          onChange={handleChange("password")}
+          endAdornment={
+            /* Password Visibility Icon Container*/
+
+            <InputAdornment position="end">
+              {/* Toggle Password Visibility Icon */}
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+
+      {/* Forgot password link container */}
+      <Box width={1} align="center" mt={5} mb={8}>
+        {/* Forgot password link */}
+        <Link to="/forgot" className={classes.linkLight}>
+          Forgot your password?
+        </Link>
+      </Box>
+
+      {/* Sign in button container */}
+      <Box width={1} align="center">
+        {/* Sign in button */}
+        <Button
+          onClick={handleLogin(values.password)}
+          variant="contained"
+          className={clsx(classes.margin, "btn btn-success")}
+        >
+          <Box px={8}>Sign in</Box>
+        </Button>
+      </Box>
+    </div>
+    /*<div className={classes.root}>
+
+
       <input
-        className="main-input-old"
+        className={clsx(classes.margin, classes.textField)}
         type="text"
         placeholder="Email"
         onChange={e => setEmail(e.target.value)}
       />
+      <FormControl
+        fullWidth
+        className={clsx(classes.margin, classes.textField)}
+      >
+        <input
+          className="main-input-old"
+          type="password"
+          placeholder="Password"
+          onChange={e => setPassword(e.target.value)}
+        />
+      </FormControl>
+      <Box width={1} align="center" mt={5} mb={8}>
 
-      <input
-        className="main-input-old"
-        type="password"
-        placeholder="Password"
-        onChange={e => setPassword(e.target.value)}
-      />
-
-      <button className="main-btn-old" onClick={handleLogin}>
-        Login
-      </button>
-    </div>
+        <Link to="/forgot" className={classes.linkLight}>
+          Forgot your password?
+        </Link>
+      </Box>
+      <Box width={1} align="center">
+        <button
+          variant="contained"
+          className={clsx(classes.margin, "btn btn-success")}
+          onClick={handleLogin}
+        >
+          <Box px={8}>Login</Box>
+        </button>
+      </Box>
+    </div>*/
   );
 }
 
