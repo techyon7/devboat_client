c23d981cf30e7282942c202ccd6c4b0eceeac718
mkd63 <sharmaaahan15@gmail.com>
2020-05-04 22:16:32 +0530

    addded post for skill/interest

--- src/components/profile-setup/GenderDobStep.jsx
+++ src/components/profile-setup/GenderDobStep.jsx
@@ -1,117 +1,143 @@
-import React from 'react';
-import clsx from 'clsx';
-import { Formik } from 'formik';
-import { makeStyles } from '@material-ui/styles';
+import React, { Fragment, useContext } from "react";
+import clsx from "clsx";
+import { Formik } from "formik";
+import { makeStyles } from "@material-ui/styles";
 import {
-	Box,
-	Button,
-	Radio,
-	RadioGroup,
-	TextField,
-	Typography,
-	FormControlLabel,
-	FormControl,
-} from '@material-ui/core';
-import { SetupSchemaDob } from '../validations/validations';
-
+  Box,
+  Button,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography,
+  FormControlLabel,
+  FormControl
+} from "@material-ui/core";
+import { SetupSchemaDob } from "../validations/validations";
+import { GlobalContext } from "../../context/GlobalContext";
+import { POST } from "../../actions/api";
 
 // Main [component (sort of)] function
 
-const GenderDobStep = (props) => {
+const GenderDobStep = props => {
+  // React Hooks
+  const classes = useStyles();
+  const { page } = props;
+  const { setState } = props;
+  const { session } = useContext(GlobalContext);
+  const previousPage = state => {
+    setState({ page: page - 1 });
+  };
 
-	// React Hooks
-	const classes = useStyles();
-	const { page } = props;
-	const { setState } = props;
-
-	const previousPage = (state) => {
-		setState({ page: page - 1 })
-	}
-
-
-	// JSX Markup
-	return(
-		<Formik
+  // JSX Markup
+  return (
+    <Formik
       initialValues={{
-				dob: ''
+        dob: ""
       }}
-			validationSchema={SetupSchemaDob}
+      validationSchema={SetupSchemaDob}
       onSubmit={values => {
         // same shape as initial values
         console.log(values);
-      }}>
-			{ props => (
-				<form onSubmit={props.handleSubmit}>
-					<Box mb={15} width={1}>
+      }}
+    >
+      {props => (
+        <form onSubmit={props.handleSubmit}>
+          <Box mb={15} width={1}>
+            {/* Date of Birth Header Text */}
+            <Box>
+              <Typography variant="h4">Date of birth</Typography>
+              <Typography variant="body1">When were you born?</Typography>
+            </Box>
 
-						{/* Date of Birth Header Text */}
-						<Box>
-							<Typography variant="h4">
-								Date of birth
-							</Typography>
-							<Typography variant="body1">
-								When were you born?
-							</Typography>
-						</Box>
+            {/* Date of Birth Input Container */}
+            <Box width={1} className={classes.root}>
+              <TextField
+                error={props.errors.dob && props.touched.dob ? true : false}
+                name="dob"
+                type="date"
+                margin="normal"
+                id="dob"
+                value={props.values.dob}
+                onChange={props.handleChange}
+                helperText={
+                  props.errors.dob && props.touched.dob
+                    ? props.errors.dob
+                    : null
+                }
+              />
+            </Box>
+          </Box>
 
-						{/* Date of Birth Input Container */}
-						<Box width={1} className={classes.root}>
+          {/* Gender Input Container */}
+          <Box display="flex" flexDirection="column">
+            {/* Gender Header Text */}
+            <Typography variant="h4">Gender</Typography>
+            <Typography variant="body1">
+              What is your current gender identity?
+            </Typography>
 
-						<TextField
-						error={props.errors.dob && props.touched.dob ? true : false}
-						name="dob"
-						type="date"
-						margin="normal"
-						id="dob"
-						value={ props.values.dob  }
-						onChange={props.handleChange}
-						helperText={props.errors.dob && props.touched.dob ? props.errors.dob : null}
-						 />
+            {/* Gender Input Options */}
+            <FormControl
+              component="fieldset"
+              className={classes.formControl}
+              name="sex"
+            >
+              <RadioGroup
+                aria-label="gender"
+                name="sex"
+                value={props.values.sex}
+                onChange={props.handleChange}
+              >
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label="Male"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio />}
+                  label="Other"
+                />
+              </RadioGroup>
+              {/* Additional gender information input (shows when "other" is selected) */}
+              {props.values.sex === "other" ? (
+                <TextField
+                  multiline
+                  name="gender-info"
+                  label="Gender Information"
+                  className=""
+                />
+              ) : null}
+            </FormControl>
+          </Box>
 
-						</Box>
-					</Box>
-
-					{/* Gender Input Container */}
-					<Box display="flex" flexDirection="column">
-
-						{/* Gender Header Text */}
-						<Typography variant="h4">
-							Gender
-						</Typography>
-						<Typography variant="body1">
-							What is your current gender identity?
-						</Typography>
-
-						{/* Gender Input Options */}
-						<FormControl component="fieldset" className={classes.formControl} name="sex">
-			        <RadioGroup aria-label="gender" name="sex" value={props.values.sex} onChange={props.handleChange}>
-			          <FormControlLabel value="female" control={<Radio />} label="Female" />
-			          <FormControlLabel value="male" control={<Radio />} label="Male" />
-			          <FormControlLabel value="other" control={<Radio />} label="Other" />
-			        </RadioGroup>
-							{/* Additional gender information input (shows when "other" is selected) */}
-							{ (props.values.sex === "other") ? <TextField multiline name="gender-info" label="Gender Information" className="" /> : null}
-			      </FormControl>
-
-
-					</Box>
-
-					{/* Buttons container */}
-					<Box mt={15} width={1} display="flex" justifyContent="space-between">
-						<Button variant="contained" className={clsx(classes.margin)} onClick={previousPage}>
-							<Box px={8}>Back</Box>
-						</Button>
-						<Button type="submit" variant="contained" className={clsx(classes.margin + "btn btn-success")}>
-							<Box px={8}>Done</Box>
-						</Button>
-					</Box>
-				</form>
-
-			)}
-
-		</Formik>
-	);
-}
+          {/* Buttons container */}
+          <Box mt={15} width={1} display="flex" justifyContent="space-between">
+            <Button
+              variant="contained"
+              className={clsx(classes.margin)}
+              onClick={previousPage}
+            >
+              <Box px={8}>Back</Box>
+            </Button>
+            <Button
+              type="submit"
+              variant="contained"
+              className={clsx(classes.margin + "btn btn-success")}
+            >
+              <Box px={8}>Done</Box>
+            </Button>
+          </Box>
+        </form>
+      )}
+    </Formik>
+  );
+};
 
 export default GenderDobStep;
 
@@ -119,25 +145,25 @@ export default GenderDobStep;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
-		flexGrow: 1
+    flexGrow: 1
   },
   chips: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   chip: {
-    margin: 2,
+    margin: 2
   },
   noLabel: {
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing(3)
   },
-	flexGrow: {
-		flexGrow: 1,
-	}
+  flexGrow: {
+    flexGrow: 1
+  }
 }));
--- src/components/profile-setup/SkillsInterestsStep.jsx
+++ src/components/profile-setup/SkillsInterestsStep.jsx
@@ -1,108 +1,136 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { skillObjects } from '../../data/skillsArray.js';
-import { SetupSchemaSkills } from '../validations/validations'
-import { Formik } from 'formik';
-import { DownshiftMultiple } from './IntegrationDownshift';
-import { makeStyles } from '@material-ui/styles';
-import {
-	Box,
-	Typography,
-	Button,
-} from '@material-ui/core';
+import React, { Fragment, useContext } from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { skillObjects } from "../../data/skillsArray.js";
+import { SetupSchemaSkills } from "../validations/validations";
+import { Formik } from "formik";
+import { DownshiftMultiple } from "./IntegrationDownshift";
+import { makeStyles } from "@material-ui/styles";
+import { Box, Typography, Button } from "@material-ui/core";
+import { GlobalContext } from "../../context/GlobalContext";
+import { POST } from "../../actions/api";
 
-const SkillsInterestsStep = (props) => {
+const SkillsInterestsStep = props => {
+  // React hooks
+  const classes = useStyles();
+  const { onSubmit } = props;
+  //const [skill, setSkill] = React.useState("");
+  //const [interest, setInterest] = React.useState("");
+  const skills = skillObjects;
+  const { session } = useContext(GlobalContext);
+  let disabled;
+  const handleSkillSubmit = async interest => {
+    let body = JSON.stringify({
+      interest_name: interest,
+      user_id: session.userId
+    });
 
-	// React hooks
-		const classes = useStyles();
-		const { onSubmit } = props;
+    const response = await POST("/auth/Interests", body);
+    const result = await response.json();
 
-		const skills = skillObjects;
-		let disabled;
-	return(
-		<Formik
+    console.log(result);
+  };
+  const handleInterestSubmit = async skills => {
+    let body = JSON.stringify({
+      skill_name: skills,
+      user_id: session.userId
+    });
+
+    const response = await POST("/auth/Skills", body);
+    const result = await response.json();
+
+    console.log(result);
+  };
+  return (
+    <Formik
       initialValues={{
-        skills: '',
-				interests: ''
+        skills: "",
+        interests: ""
       }}
-			validationSchema={SetupSchemaSkills}
+      validationSchema={SetupSchemaSkills}
       onSubmit={values => {
         // same shape as initial values
+        handleSkillSubmit(values.skills);
+        handleInterestSubmit(values.interests);
         console.log(values);
       }}
+      render={props => (
+        <form onSubmit={onSubmit}>
+          <Box mb={15} width={1}>
+            <Typography variant="h4">Skills</Typography>
+            <Typography variant="body1">
+              What are some things that you're good at?
+            </Typography>
 
-		render={(props) => (
-			<form onSubmit={onSubmit}>
+            <DownshiftMultiple
+              placeholder="Search for skills"
+              label="Skills"
+              name="skills"
+              options={skills}
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={
+                props.values !== undefined ? props.values.skills : undefined
+              }
+            />
 
-					<Box mb={15} width={1}>
-						<Typography variant="h4">
-							Skills
-						</Typography>
-						<Typography variant="body1">
-							What are some things that you're good at?
-						</Typography>
-
-						<DownshiftMultiple
-						placeholder="Search for skills"
-						label="Skills"
-						name="skills"
-						options={skills}
-						onChange={props.handleChange}
+            {/* Skills */}
+          </Box>
+          <Typography variant="h4">Interests</Typography>
+          <Typography variant="body1">
+            What are some things that you'd like to learn?
+          </Typography>
+          <DownshiftMultiple
+            placeholder="Search for interests"
+            label="Interests"
+            options={skills}
+            name="interests"
+            error={
+              props.errors.interests && props.touched.interests !== undefined
+                ? "error"
+                : undefined
+            }
+            helperText={props.errors.interests && props.touched.interests}
+            onChange={e => {
+              props.handleChange(e);
+              console.log(props.values.interests);
+            }}
             onBlur={props.handleBlur}
-            value={props.values !== undefined ? props.values.skills : undefined}
-						/>
-
-						{/* Skills */}
-					</Box>
-					<Typography variant="h4">
-						Interests
-					</Typography>
-					<Typography variant="body1">
-						What are some things that you'd like to learn?
-					</Typography>
-					<DownshiftMultiple
-					placeholder="Search for interests"
-					label="Interests"
-					options={skills}
-					name="interests"
-					error={props.errors.interests && props.touched.interests !== undefined ? "error" : undefined}
-					helperText={props.errors.interests && props.touched.interests}
-					onChange={(e) => {props.handleChange(e); console.log(props.values.interests)}}
-					onBlur={props.handleBlur}
-					value={props.values.interests}
-					/>
-					<Box mt={15}>
-						<Button type="submit" variant="contained" className={clsx(classes.margin + "btn btn-success")} {...props.values.interests.length > 0 ? undefined : disabled}>
-							<Box px={8}>Next</Box>
-						</Button>
-					</Box>
-			</form>
-
-		)}
-		/>
-
-	);
-}
+            value={props.values.interests}
+          />
+          <Box mt={15}>
+            <Button
+              type="submit"
+              variant="contained"
+              className={clsx(classes.margin + "btn btn-success")}
+              {...(props.values.interests.length > 0 ? undefined : disabled)}
+            >
+              <Box px={8}>Next</Box>
+            </Button>
+          </Box>
+        </form>
+      )}
+    />
+  );
+};
 
 export default SkillsInterestsStep;
 
 SkillsInterestsStep.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-	page: PropTypes.number.isRequired,
-
-}
+  page: PropTypes.number.isRequired
+};
 
 // Styles
 
 const useStyles = makeStyles(theme => ({
-	root: {
-		flexGrow: 1,
-	},
-	margin: {
-    margin: theme.spacing(1),
+  root: {
+    flexGrow: 1
+  },
+  margin: {
+    margin: theme.spacing(1)
   },
   boxPrimary: {
-    background: '#4B7BEC'
-  },
+    background: "#4B7BEC"
+  }
 }));
