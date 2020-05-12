b52964b9e04cc9b63bff2025b4209405367899f1
Sparsh Tyagi <sparshtyagi21@gmail.com>
2020-05-12 21:53:00 +0200

    Add education functionality

--- src/components/user-profile/EducationSettings.jsx
+++ src/components/user-profile/EducationSettings.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState, useContext, useCallback } from "react";
 import clsx from "clsx";
 import { Formik } from "formik";
 import { makeStyles } from "@material-ui/core/styles";
@@ -13,40 +13,51 @@ import {
 } from "@material-ui/core";
 import {
   Box,
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  Input,
-  InputAdornment,
   Button
 } from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
-import DoneIcon from "@material-ui/icons/Done";
 import AddBoxOutlinedIcon from "@material-ui/icons/AddBoxOutlined";
 import { POST, GET } from "../../actions/api";
 import { GlobalContext } from "../../context/GlobalContext";
 
-const EducationSettings = props => {
+const EducationSettings = () => {
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    showEducation: false,
-    institution: "",
-    qualification: "",
-    start: "",
-    end: "",
-    currentlyStudying: true
-  });
-  const { onSubmit } = props;
   const { session } = useContext(GlobalContext);
-  const handleClickEducation = () => {
-    const { showEducation } = state;
+
+	const [educations, setEducations] = useState([]);
+  const [state, setState] = useState({
+    institution_name: "",
+    qualification_name: "",
+    start_date: null,
+    end_date: null,
+    currently_studying: true
+  });
+  const [showEducation, setShowEducation] = useState(false);
+
+  const loadEducations = useCallback(async () => {
+    const response = await GET('/education', session.token);
+    const result = await response.json();
+    let educations = [];
+    for (let i = 0; i < result.length; i++) {
+      if (result[i].user === session.userId) {
+        educations = [...educations, result[i]];
+      }
+    }
+    setEducations(educations);
+  }, [session.userId, session.token]);
+
+  useEffect(() => {
+    loadEducations();
+  }, [loadEducations]);
+
+  const handleAddEducation = () => {
+    setShowEducation(!showEducation);
     setState({
-      showEducation: !showEducation,
-      institution: "",
-      qualification: "",
-      start: "",
-      end: "",
-      currentlyStudying: true
+      institution_name: "",
+      qualification_name: "",
+      start_date: null,
+      end_date: null,
+      currently_studying: true
     });
   };
 
@@ -54,76 +65,34 @@ const EducationSettings = props => {
     setState({ ...state, [name]: event.target.checked });
   };
 
-  const prefillInput = () => {
-    setState({
-      showEducation: true,
-      institution: "Delhi Public School, Ranchi",
-      qualification: "Foundation degree in Natural Sciences",
-      start: "2016-04-01",
-      end: "",
-      currentlyStudying: true
-    });
-  };
-  const getEduaction = async () => {
-    const response = await GET("/aducation", session.token);
-    const result = await response.json();
-    console.log(result);
-  };
-  const handleSubmit = async (
-    institution,
-    qualification,
-    startDate,
-    endDate,
-    currentlyStudying
-  ) => {
-    const { showEducation } = state;
-    setState({
-      showEducation: !showEducation,
-      institution: "",
-      qualification: "",
-      start: "",
-      end: "",
-      currentlyStudying: true
-    });
+  const handleSubmit = async (data) => {
     let body = {
-      institution_name: institution,
-      qualification_name: qualification,
-      start_date: startDate,
-      end_date: endDate,
-      currently_studying: currentlyStudying,
+      institution_name: data.institution_name,
+      qualification_name: data.qualification_name,
+      start_date: data.start_date,
+      end_date: data.end_date,
+      currently_studying: data.currently_studying,
       user: session.userId
     };
 
-    const response = await POST("/education", body, session.token);
-    const result = await response.json();
-
-    console.log(result);
-    return response.status;
+    await POST("/education", body, session.token);
+    setShowEducation(!showEducation);
+    loadEducations();
   };
+
   return (
     <React.Fragment>
-      {state.showEducation ? (
+      {showEducation && (
         <ListItem>
           <Formik
             initialValues={{
-              institution: state.institution,
-              qualification: state.qualification,
-              start: state.start,
-              end: state.end,
-              currentlyStudying: state.currentlyStudying
+              institution_name: state.institution_name,
+              qualification_name: state.qualification_name,
+              start_date: state.start_date,
+              end_date: state.end_date,
+              currently_studying: state.currently_studying
             }}
-            //validationSchema={SetupSchemaSkills}
-            onSubmit={values => {
-              handleSubmit(
-                values.institution,
-                values.qualification,
-                values.start,
-                values.end,
-                values.currentlyStudying
-              );
-              // same shape as initial values
-              console.log(values);
-            }}
+            onSubmit={values => handleSubmit(values)}
             render={props => (
               <form
                 onSubmit={props.handleSubmit}
@@ -132,37 +101,29 @@ const EducationSettings = props => {
                 <TextField
                   placeholder="Institution"
                   label="Institution"
-                  name="institution"
+                  name="institution_name"
                   onChange={props.handleChange}
                   onBlur={props.handleBlur}
-                  value={
-                    props.values !== undefined
-                      ? props.values.institution
-                      : undefined
-                  }
+                  value={props.values.institution_name}
                 />
                 <TextField
                   placeholder="Qualification"
                   label="Qualification"
-                  name="qualification"
+                  name="qualification_name"
                   onChange={props.handleChange}
                   onBlur={props.handleBlur}
-                  value={
-                    props.values !== undefined
-                      ? props.values.qualification
-                      : undefined
-                  }
+                  value={props.values.qualification_name}
                 />
                 <TextField
                   error={
                     props.errors.start && props.touched.start ? true : false
                   }
-                  name="start"
+                  name="start_date"
                   label="Start Date"
                   type="date"
                   margin="normal"
                   id="start"
-                  value={props.values.start}
+                  value={props.values.start_date}
                   onChange={props.handleChange}
                   helperText={
                     props.errors.start && props.touched.start
@@ -173,17 +134,15 @@ const EducationSettings = props => {
                     shrink: true
                   }}
                 />
-                {state.currentlyStudying ? (
-                  ""
-                ) : (
+                {!state.currently_studying && (
                   <TextField
                     error={props.errors.end && props.touched.end ? true : false}
-                    name="end"
+                    name="end_date"
                     label="End Date"
                     type="date"
                     margin="normal"
                     id="end"
-                    value={props.values.end}
+                    value={props.values.end_date}
                     onChange={props.handleChange}
                     helperText={
                       props.errors.end && props.touched.end
@@ -198,9 +157,9 @@ const EducationSettings = props => {
                 <FormControlLabel
                   control={
                     <Checkbox
-                      checked={state.currentlyStudying}
-                      onChange={handleChange("currentlyStudying")}
-                      value="currentlyStudying"
+                      checked={state.currently_studying}
+                      onChange={handleChange("currently_studying")}
+                      value="currently_studying"
                       color="primary"
                     />
                   }
@@ -220,47 +179,49 @@ const EducationSettings = props => {
             )}
           />
         </ListItem>
-      ) : (
-        ""
       )}
-      <ListItem
-        button
-        component="a"
-        className={classes.addEducation}
-        onClick={handleClickEducation}
-      >
-        <ListItemText
-          id="switch-list-label"
-          primary={!state.showEducation && "Add a qualification"}
-        />
-        <ListItemSecondaryAction className={classes.addEducation}>
-          <IconButton
-            className={classes.addEducation}
-            onClick={handleClickEducation}
-          >
-            {!state.showEducation && <AddBoxOutlinedIcon />}
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
-      <ListItem>
-        <ListItemText
-          id="switch-list-label"
-          primary="Delhi Public School, Ranchi"
-          secondary="Foundation Course in Natural Sciences"
-        />
-        <ListItemSecondaryAction>
-          <IconButton>
-            <EditIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
+      {!showEducation && (
+        <ListItem
+          button
+          component="a"
+          className={classes.addEducation}
+          onClick={handleAddEducation}
+        >
+          <ListItemText
+            id="switch-list-label"
+            primary="Add a qualification"
+          />
+          <ListItemSecondaryAction className={classes.addEducation}>
+            <IconButton
+              className={classes.addEducation}
+              onClick={handleAddEducation}
+            >
+              <AddBoxOutlinedIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      )}
+      {educations.map((education) => (
+        <ListItem>
+          <ListItemText
+            id="switch-list-label"
+            primary={education.institution_name}
+            secondary={education.qualification_name}
+          />
+          <ListItemSecondaryAction>
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))}
     </React.Fragment>
   );
 };
 
 export default EducationSettings;
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   addEducation: {
     color: "#4b7bec"
   },
