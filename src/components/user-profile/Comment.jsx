705adc39a3420e6ffb1f2f036f8dd2aceb4c07da
Sparsh Tyagi <sparshtyagi21@gmail.com>
2020-05-23 21:28:10 +0200

    add comments list

---
+++ src/components/user-profile/Comment.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useContext } from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+	Grid,
+	Box,
+	Button,
+	Paper,
+	Typography
+} from '@material-ui/core';
+import UserPicture from "./UserPicture";
+import ExpandLessRoundedIcon from '@material-ui/icons/ExpandLessRounded';
+import ForumRoundedIcon from '@material-ui/icons/ForumRounded';
+import { GlobalContext } from "../../context/GlobalContext";
+import { POST, GET, DELETE } from "../../actions/api";
+
+export default function Comment(props) {
+	const classes = useStyles();
+  const { session } = useContext(GlobalContext);
+
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		(async () => {
+			const response = await GET(`/users/${props.userId}`, session.token);
+			const result = await response.json();
+			setUser(result);
+		})();
+	}, [props.userId, session.token]);
+
+	return(
+		<Box p={4}>
+			{user &&
+				<Box display="flex" justifyContent="left">
+					<Box className={classes.userPic}>
+						<UserPicture picture={user.picture} crop={user.cropped_data}/>
+					</Box>
+					<Box>
+						<Typography variant="body1" color="textPrimary" className={classes.title}>
+							{`${user.first_name} ${user.last_name}`}
+						</Typography>
+						<Typography textAlign="left" variant="body1" color="textSecondary" className={classes.small}>
+							{props.content}
+						</Typography>
+					</Box>
+				</Box>
+			}
+		</Box>
+	);
+}
+
+const useStyles = makeStyles(theme => ({
+	userPic: {
+		width: 24,
+		height: 24,
+		marginRight: 8,
+		cursor: 'pointer'
+	},
+	title: {
+		fontSize: "0.80rem",
+		cursor: 'pointer',
+		marginBottom: 4
+	},
+	small: {
+		fontSize: "0.75rem"
+	}
+}));
--- src/components/user-profile/Post.jsx
+++ src/components/user-profile/Post.jsx
@@ -9,16 +9,37 @@ import {
 	Typography
 } from '@material-ui/core';
 import UserPicture from "./UserPicture";
+import ExpandLessRoundedIcon from '@material-ui/icons/ExpandLessRounded';
+import ForumRoundedIcon from '@material-ui/icons/ForumRounded';
+import Comments from "./Comments";
 import { GlobalContext } from "../../context/GlobalContext";
 import { POST, GET, DELETE } from "../../actions/api";
 
-export default function Posts(props) {
+export default function Post(props) {
 	const classes = useStyles();
   const { session } = useContext(GlobalContext);
 
 	const [upvotes, setUpvotes] = useState([]);
 	const [upvotesCount, setUpvotesCount] = useState(0);
 	const [upvoteId, setUpvoteId] = useState(null);
+
+	const [comments, setComments] = useState([]);
+	const [commentsCount, setCommentsCount] = useState(0);
+
+	useEffect(() => {
+		(async () => {
+			const response = await GET('/comments', session.token);
+			const result = await response.json();
+			let comments = [];
+			for (let i = 0; i < result.length; i++) {
+				if (result[i].post === props.id) {
+					comments = [...comments, result[i]];
+				}
+			}
+			setComments(comments);
+			setCommentsCount(comments.length);
+		})();
+	}, [session.token]);
 
 	useEffect(() => {
 		(async () => {
@@ -58,33 +79,45 @@ export default function Posts(props) {
 	return(
 		<Grid className={classes.item} item xs={12}>
 			<Paper elevation={false} className={classes.paper}>
-				<Box mb={4} display="flex" alignItems="center" justifyContent="left">
-					<Box className={classes.userPic}>
-						<UserPicture picture={props.userPicture} crop={props.userCrop}/>
+				<Box p={4} pb={0}>
+					<Box mb={4} display="flex" alignItems="center" justifyContent="left">
+						<Box className={classes.userPic}>
+							<UserPicture picture={props.userPicture} crop={props.userCrop}/>
+						</Box>
+						<Box className={classes.title}>
+							<Typography variant="body1" color="textPrimary">
+								{`${props.userFirstName} ${props.userLastName}`}
+							</Typography>
+							<Typography variant="body1" color="textSecondary" className={classes.small}>
+								{(new Date(props.date)).toUTCString()}
+							</Typography>
+						</Box>
 					</Box>
-					<Box className={classes.title}>
-						<Typography variant="body1" color="textPrimary">
-							{`${props.userFirstName} ${props.userLastName}`}
-						</Typography>
-						<Typography variant="body1" color="textSecondary" className={classes.small}>
-							{(new Date(props.date)).toUTCString()}
-						</Typography>
+					<Typography textAlign="left" variant="body1" color="textSecondary">
+						{props.content}
+					</Typography>
+					<Box mt={4} mb={4} display="flex" alignItems="center" justifyContent="left">
+						<Button
+							className={clsx(classes.button, {
+								[classes.active]: upvoteId
+							})}
+							onClick={upvoteId ? deleteUpvote : handleUpvote}>
+							<ExpandLessRoundedIcon style={{ fontSize: 20 }}/>
+							<Typography variant="body1" color="textSecondary">
+								{upvotesCount}
+							</Typography>
+						</Button>
+						<Button
+							className={classes.button}>
+							<ForumRoundedIcon style={{ fontSize: 18 }}/>
+							<Typography variant="body1" color="textSecondary">
+								{commentsCount}
+							</Typography>
+						</Button>
 					</Box>
 				</Box>
-				<Typography textAlign="left" variant="body1" color="textSecondary">
-					{props.content}
-				</Typography>
-				<Box mt={4} display="flex" alignItems="center" justifyContent="left">
-					<Button
-						className={clsx(classes.upvote, {
-							[classes.active]: upvoteId
-						})}
-						onClick={upvoteId ? deleteUpvote : handleUpvote}>
-						+1
-					</Button>
-					<Typography variant="body1" color="textSecondary">
-						{upvotesCount}
-					</Typography>
+				<Box>
+					<Comments comments={comments} />
 				</Box>
 			</Paper>
 		</Grid>
@@ -93,7 +126,6 @@ export default function Posts(props) {
 
 const useStyles = makeStyles(theme => ({
 	paper: {
-		padding: theme.spacing(4, 3.5, 3, 4),
 		color: theme.palette.text.primary,
 		backgroundColor: theme.palette.secondary.main
 	},
@@ -106,16 +138,18 @@ const useStyles = makeStyles(theme => ({
 		marginRight: 8,
 		cursor: 'pointer'
 	},
-	upvote: {
-		minWidth: 34,
-		width: 34,
+	button: {
+		minWidth: 48,
+		width: 60,
 		height: 34,
 		fontSize: 14,
 		letterSpacing: 1.1,
 		marginRight: 8,
 		backgroundColor: theme.palette.secondary.vice,
-		borderRadius: "50%",
-		color: theme.palette.text.secondary,
+		borderRadius: "16px",
+		textAlign: 'left',
+		color: theme.palette.text.primary,
+		justifyContent: 'space-around'
 	},
 	active: {
 		backgroundColor: theme.palette.primary.main,
