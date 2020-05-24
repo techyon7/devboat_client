ed35a40be4260ea7699d5aca28f8a60788804c18
Sparsh Tyagi <sparshtyagi21@gmail.com>
2020-05-24 06:38:50 +0200

    change devboat description design

--- src/components/authentication/DevboatDescription.jsx
+++ src/components/authentication/DevboatDescription.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
-import { Container, Box, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from "react-router-dom";
+import { Box, Typography } from '@material-ui/core';
+import logo from '../../assets/logo192.png';
 
 export default function DevboatDescription() {
-	return(
-		<Container maxWidth="sm">
+	const classes = useStyles();
+
+	return (
+		<Box className={classes.root}>
+			<Link to="/">
+				<img className={classes.logo} alt="logo" src={logo}/>
+			</Link>
+
 			<Box mb={5}>
-				<Typography variant="h3">
+				<Typography variant="h4">
 					A social network for developers
 				</Typography>
 			</Box>
 
 			<Typography variant="body1">
 				<Box component="span">
-					DevBoat is a networking platform built specifically for developers. Our goal was to build a platform where developers could interact with each other. Whether it is to learn to code as a beginner or build a project as a veteran developer.
+					<span className={classes.name}>DevBoat</span> is a networking platform built specifically for developers. Our goal was to build a platform where developers could interact with each other. Whether it is to learn to code as a beginner or build a project as a veteran developer.
 				</Box>
 			</Typography>
-		</Container>
+		</Box>
 	);
 }
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		width: "70%",
+		margin: "0 auto"
+	},
+  logo: {
+    height: 100,
+    width: 100,
+		marginBottom: 10
+  },
+	name: {
+		color: theme.palette.primary.main
+	}
+}));
