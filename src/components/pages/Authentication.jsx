03101b7708cd88eeb8c157110cafe64bd38118ca
Sparsh Tyagi <sparshtyagi21@gmail.com>
2020-05-24 06:38:30 +0200

    structural change authentication

--- src/components/pages/Authentication.jsx
+++ src/components/pages/Authentication.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useContext } from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import AuthButtonGroup from "../authentication/AuthButtonGroup";
 import DevboatDescription from "../authentication/DevboatDescription";
+import LoginRegister from "../authentication/LoginRegister";
 import Login from "../authentication/Login";
 import Register from "../authentication/Register";
 import VerifyEmail from "../authentication/VerifyEmail";
@@ -34,6 +34,20 @@ export default function Authentication(props) {
         <div className={classes.root}>
           <Router>
             <Grid container spacing={0}>
+              <Hidden mdDown>
+                <Grid item xs={12} lg={6}>
+                  <Box
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    minHeight="100vh"
+                    width="100%"
+                  >
+                    <DevboatDescription />
+                  </Box>
+                </Grid>
+              </Hidden>
               <Grid item xs={12} lg={6}>
                 <Box
                   display="flex"
@@ -41,49 +55,22 @@ export default function Authentication(props) {
                   alignItems="center"
                   justifyContent="center"
                   minHeight="100vh"
-                  width={1}
-                  px={3}
+                  width="100%"
                 >
-                  <Container maxWidth="sm">
-                    <Box mb={5}>
-                      <Route
-                        path="/(|login|register)"
-                        render={({ match }) => (
-                          <Fragment>
-                            {match.isExact ? <AuthButtonGroup /> : null}
-                          </Fragment>
-                        )}
-                      />
-                    </Box>
-                    <Route path="/(login|)" render={(props) => (
-                      <Login {...props}
-                        loggedIn={loggedIn}/>
-                      )}
-                    />
-                    <Route path="/register" component={Register} />
-                    <Route path="/confirm" component={ConfirmEmail} />
-                    <Route path="/verify/:verification_key" component={VerifyEmail} />
-                    <Route path="/forgot" component={Forgot} />
-                    <Route path="/forgot-sent" component={ForgotSent} />
-                    <Route path="/reset/:reset_password_key" component={Reset} />
-                    <Route path="/reset-done" component={ResetDone} />
-                  </Container>
+                  <Route path="/(login|)" render={(props) => (
+                    <LoginRegister {...props}
+                      loggedIn={loggedIn} />
+                    )}
+                  />
+                <Route path="/register" component={LoginRegister}/>
+                  <Route path="/confirm" component={ConfirmEmail} />
+                  <Route path="/verify/:verification_key" component={VerifyEmail} />
+                  <Route path="/forgot" component={Forgot} />
+                  <Route path="/forgot-sent" component={ForgotSent} />
+                  <Route path="/reset/:reset_password_key" component={Reset} />
+                  <Route path="/reset-done" component={ResetDone} />
                 </Box>
               </Grid>
-
-              <Hidden mdDown>
-                <Grid item xs={12} lg={6} className={classes.boxPrimary}>
-                  <Box
-                    display="flex"
-                    flexDirection="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    minHeight="100vh"
-                  >
-                    <DevboatDescription />
-                  </Box>
-                </Grid>
-              </Hidden>
             </Grid>
           </Router>
         </div>
