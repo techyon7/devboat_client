d6def56744f71328e6162d22963f4b8a435e3130
mkd63 <sharmaaahan15@gmail.com>
2020-06-06 11:24:39 +0530

    added default text for no posts

--- src/components/user-profile/Posts.jsx
+++ src/components/user-profile/Posts.jsx
@@ -1,7 +1,9 @@
 import React, { Fragment } from "react";
 import Post from "../user-profile/Post";
-
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper, Typography } from "@material-ui/core";
 export default function Posts(props) {
+  const classes = useStyles();
   return (
     <Fragment>
       {props.posts.map(item => (
@@ -17,6 +19,32 @@ export default function Posts(props) {
           loadPosts={props.loadPosts}
         />
       ))}
+      {props.posts <= 0 && (
+        <Grid className={classes.item} item xs={12}>
+          <Paper elevation={false} className={classes.paper}>
+            <Typography
+              style={{
+                textAlign: "center",
+                margin: 10,
+                fontSize: 14
+              }}
+            >
+              No posts to show
+            </Typography>
+          </Paper>
+        </Grid>
+      )}
     </Fragment>
   );
 }
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(4, 0, 4, 0),
+    color: theme.palette.text.primary,
+    backgroundColor: theme.palette.secondary.main
+  },
+  item: {
+    marginBottom: "0.625rem"
+  }
+}));
